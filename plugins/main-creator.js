c0bc84e53bf89922feaefb4c40ee37c773681c0c
Ado <minexdt@gmail.com>
2025-07-19 12:09:05 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,24 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Sofi 👸🏻;;\nFN:Sofi 👸🏻\nORG:Sofi 👸🏻\nTITLE:\nitem1.TEL;waid=50765609370:50765609370\nitem1.X-ABLabel:Sofi 👸🏻\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Sofi 👸🏻\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+let handler = async (m, { conn }) => {
+  let vcard = `BEGIN:VCARD
+VERSION:3.0
+N:Ado;;;
+FN:Ado
+ORG:DOGE Bot
+TITLE:Creador
+item1.TEL;waid=50493732693:+50493732693
+item1.X-ABLabel:Creador del DOGE Bot
+END:VCARD`;
 
-export default handler
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: 'Ado - DOGE Bot',
+      contacts: [{ vcard }]
+    }
+  }, { quoted: m });
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
