f3f3a383a6701cb6f449b9040bfb7c68e9398f13
Ado <minexdt@gmail.com>
2025-07-19 11:10:19 -0600

    Update downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,22 +1,44 @@
 import Scraper from "@SumiFX/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🍭 Ingresa el nombre de la aplicación que deseas descargar.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* WhatsApp`)
+  if (!text) return m.reply(`
+┃╹★ 🍬 *Poné el nombre de la app que querés bajar* 🍬
+┃╹★
+┃╹★ 📝 *Ejemplo:*
+┃╹★ > *${usedPrefix + command}* WhatsApp
+`)
 
-try {
-let { name, packname, update, size, thumbnail, dl_url } = await Scraper.aptoide(text)
-if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
-let txt = `╭─⬣「 *Aptoide Download* 」⬣\n`
-    txt += `│  ≡◦ *🍭 Nombre ∙* ${name}\n`
-    txt += `│  ≡◦ *🪴 Packname ∙* ${packname}\n`
-    txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`
-    txt += `│  ≡◦ *🕜 Peso ∙* ${update}\n`
-    txt += `╰─⬣`
-await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
-await conn.sendMessage(m.chat, {document: { url: dl_url }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: null }, {quoted: m})
-} catch {
-}}
-handler.help = ['aptoide <búsqueda>']
+  try {
+    let { name, packname, update, size, thumbnail, dl_url } = await Scraper.aptoide(text)
+
+    if (size.includes('GB') || parseFloat(size.replace(' MB', '')) > 300)
+      return await m.reply('┃╹★ ⚠️ *La app pesa más de 300 MB, cancelé la descarga pa que no te pete el celu* ⚠️')
+
+    let message = `
+┃╹★ 🍭 *Info de la app:*          
+┃╹★ ────────────────
+┃╹★ 📌 *Nombre:* ${name}
+┃╹★ 🆔 *Packname:* ${packname}
+┃╹★ ⚖️ *Tamaño:* ${size}
+┃╹★ ⏰ *Actualizado:* ${update}
+`
+
+    await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', message, m)
+    await conn.sendMessage(
+      m.chat,
+      {
+        document: { url: dl_url },
+        mimetype: 'application/vnd.android.package-archive',
+        fileName: `${name}.apk`,
+      },
+      { quoted: m }
+    )
+  } catch {
+    await m.reply('┃╹★ ❌ *No encontré la app, prueba con otro nombre o revisa la ortografía* ❌')
+  }
+}
+
+handler.help = ['apk <búsqueda>']
 handler.tags = ['downloader']
 handler.command = ['aptoide', 'apk']
 //handler.limit = 5
