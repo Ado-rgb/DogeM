4832040d751af0d83af41dc6e70722eb6293c021
Ado <minexdt@gmail.com>
2025-07-19 13:09:06 -0600

    Actualizar group-config.js

--- plugins/group-config.js
+++ plugins/group-config.js
@@ -1,6 +1,5 @@
-let handler = async (m, {conn, args, usedPrefix, command}) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   let isClose = {
-    // Switch Case Like :v
     open: "not_announcement",
     close: "announcement",
     abierto: "not_announcement",
@@ -8,22 +7,19 @@ let handler = async (m, {conn, args, usedPrefix, command}) => {
     abrir: "not_announcement",
     cerrar: "announcement",
   }[args[0] || ""];
-  if (isClose === undefined)
-    throw `
 
-*╭─ ❖ ── ✦ ── ✧ ── ❖ ──┓* 
-*┠┉↯ ${usedPrefix + command} abrir*
-*┠┉↯ ${usedPrefix + command} cerrar*
-*╰─ ❖ ── ✦ ── ✧ ── ❖ ──┛*
-`.trim();
-  await conn.groupSettingUpdate(m.chat, isClose);
-  {
-    m.reply("☁️ 𝘎𝘳𝘶𝘱𝘰 𝘊𝘰𝘯𝘧𝘪𝘨𝘶𝘳𝘢𝘥𝘰 𝘊𝘰𝘳𝘳𝘦𝘤𝘵𝘢𝘮𝘦𝘯𝘵𝘦");
+  if (isClose === undefined) {
+    throw `Usa:\n> ${usedPrefix + command} abrir\n> ${usedPrefix + command} cerrar`;
   }
+
+  await conn.groupSettingUpdate(m.chat, isClose);
+  m.reply("✅ Grupo configurado correctamente");
 };
+
 handler.help = ["group open / close", "grupo abrir / cerrar"];
 handler.tags = ["group"];
 handler.command = /^(group|grupo)$/i;
 handler.admin = true;
 handler.botAdmin = true;
+
 export default handler;
