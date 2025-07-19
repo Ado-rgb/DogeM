c427fb4c9722cff1c71693898841728122f16e36
Ado <minexdt@gmail.com>
2025-07-19 12:26:32 -0600

    Actualizar img-image.js

--- plugins/img-image.js
+++ plugins/img-image.js
@@ -1,20 +1,28 @@
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `*⚠️ Uso Correcto: ${usedPrefix + command} Extrella dorada*`, m, rcanal);
-conn.reply(m.chat, '🦋 *Descargando su imagen...*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-const res = await googleImage(text);
-const image = await res.getRandom();
-const link = image;
-const messages = [['Imagen 1', dev, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, `🧸 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+import { googleImage } from '@bochilteam/scraper';
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `*⚠️ Uso Correcto: ${usedPrefix + command} Estrella dorada*`, m);
+
+  await conn.reply(m.chat, '🦋 Descargando su imagen...', m);
+
+  const res = await googleImage(text);
+  const image1 = await res.getRandom();
+  const image2 = await res.getRandom();
+  const image3 = await res.getRandom();
+  const image4 = await res.getRandom();
+
+  const messages = [
+    ['Imagen 1', 'Descarga', image1, [[]], [[]], [[]], [[]]],
+    ['Imagen 2', 'Descarga', image2, [[]], [[]], [[]], [[]]],
+    ['Imagen 3', 'Descarga', image3, [[]], [[]], [[]], [[]]],
+    ['Imagen 4', 'Descarga', image4, [[]], [[]], [[]], [[]]],
+  ];
+
+  await conn.sendCarousel(m.chat, `🧸 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
 };
-handler.help = ['imagen + Texto'];
+
+handler.help = ['imagen + texto'];
 handler.tags = ['downloader'];
 handler.command = ['image', 'imagen'];
+
 export default handler;
