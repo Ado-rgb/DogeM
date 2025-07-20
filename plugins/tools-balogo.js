9fd508ba8f6b8432498b2904f5dfa9972115446a
Ado <minexdt@gmail.com>
2025-07-19 21:17:35 -0600

    Crear tools-balogo.js

---
+++ plugins/tools-balogo.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+  if (args.length < 2) return m.reply(`*Por favor ingresa el comando más el texto uno y dos.*\n> *\`Ejemplo:\`* ${usedPrefix + command} Ado`);
+  await conn.sendMessage(m.chat, {
+    image: {
+      url: 'https://api.nekorinn.my.id/maker/ba-logo?textL=' + encodeURIComponent(args[0]) + '&textR=' + encodeURIComponent(args.slice(1).join(' '))
+    }
+  }, { quoted: m });
+};
+
+handler.help = ['balogo'];
+handler.command = ['balogo'];
+handler.tags = ['logos']
+
+export default handler;
